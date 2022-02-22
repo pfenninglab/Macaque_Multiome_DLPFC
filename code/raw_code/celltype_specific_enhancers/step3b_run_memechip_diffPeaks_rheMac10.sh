8a3b8867a75351c743fb255c8cbcf26972907ee7
badoi <badoi@me.com>
2022-02-21 20:13:13 -0500

    meme-chip run

--- code/raw_code/celltype_specific_enhancers/step3b_run_memechip_diffPeaks_rheMac10.sh
+++ code/raw_code/celltype_specific_enhancers/step3b_run_memechip_diffPeaks_rheMac10.sh
@@ -15,6 +15,8 @@ DATADIR=$PROJDIR/data/tidy_data/celltype_specific_enhancers
 DBFILE=/home/bnphan/resources/motif_databases/HUMAN/HOCOMOCOv11_full_HUMAN_mono_meme_format.meme
 GENOME=/home/bnphan/resources/genomes/rheMac10/rheMac10.fa
 cd $CODEDIR
+## for Andrew
+
 
 ##################################
 ### cell type and fold index #####
